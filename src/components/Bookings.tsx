74e9a5fc91991d331d560c81f7feb9f861bdaea1
wadim baslow <wadim.baslow@posteo.de>
2022-03-04 10:54:19 +0100

    edit to booking confirmation feedback

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
-import { List, ListItem, Divider, Typography, Box } from "@mui/material"
+import { List, ListItem, Divider, Typography, Box, Chip } from "@mui/material"
 import { DateTime } from "luxon";
 import EventIcon from '@mui/icons-material/Event';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
@@ -10,7 +10,8 @@ interface Booking {
     booking_id: string,
     created_at: string,
     booking_time: string,
-    booked_location: string | null | undefined, 
+    booked_location: string | null | undefined,
+    booking_confirmed: boolean, 
     product: {
         product_description: string | null,
         product_id: string,
@@ -23,9 +24,8 @@ const Bookings = ()=>{
     const [bookings, setBookings] =useState<Booking[]>([])
 
     const fetchBookings = async ()=>{
-        const accessToken = localStorage.getItem("access_token")
         try {
-            const {data} = await axios.post("/api/bookings", {accessToken})
+            const {data} = await axios.get("/api/bookings")
             setBookings(data.booking)
         } catch (error: any) {
             console.log(error.response.data.error.message)
@@ -44,7 +44,10 @@ const Bookings = ()=>{
                 return (
                     <React.Fragment key={booking.booking_id}>
                         <ListItem  sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between' , py: 2}} disablePadding>
-                            <Typography variant='h5' component='h3' >{booking.product.product_name}</Typography>
+                            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                                <Typography variant='h5' component='h3' sx={{fontWeight: 'medium'}} >{booking.product.product_name}</Typography>
+                                <Chip label={booking.booking_confirmed ? 'Confirmed' : 'Confirmation Pending'} color={booking.booking_confirmed ? 'success' : 'warning'} variant='outlined' size="small" sx={{ml: 2}}/>
+                            </Box>
                             <Box sx={{display: 'flex', alignItems: 'center'}}>
                                 <Box  color='info.main' sx={{display: 'flex', alignItems: 'center', ml: 4, }}>
                                     <EventIcon sx={{mr: 1}}/>
